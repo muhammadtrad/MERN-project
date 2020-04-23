20e99ed3aa3ffca2031d8f15a89d7b36590c0f8c
xmagnetisx <muhammad.trad@yahoo.com>
2020-04-22 22:35:47 -0700

    setup server

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 
-
+const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Welcome to this new App!"));
 
 const port = process.env.PORT || 5000;
 
 app.listen( port, () => console.log(`Server is running on port ${port}`));
+
