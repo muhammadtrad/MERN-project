68fcd29675828882e91f6b4cdeb865933eee58d5
xmagnetisx <muhammad.trad@yahoo.com>
2020-04-24 20:05:20 -0700

    add json

--- app.js
+++ app.js
@@ -6,13 +6,23 @@ const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
+const  User = require("./models/user");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Welcome to this new App!"));
+app.get("/", (req, res) => {
+const user = new User({
+  handle: "jim",
+  email: "jim@jim.com",
+  password: "jimisgreat123"
+});
+user.save();
+res.send("Welcome to this new App!");
+});
+
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 app.use(bodyParser.urlencoded({ extended: false }));
