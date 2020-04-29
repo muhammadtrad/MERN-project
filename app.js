36f045eb5d24c6581c1aaba34f13663030432611
xmagnetisx <muhammad.trad@yahoo.com>
2020-04-28 22:04:31 -0700

    update bug

--- app.js
+++ app.js
@@ -40,3 +40,9 @@ const port = process.env.PORT || 5000;
 
 app.listen( port, () => console.log(`Server is running on port ${port}`));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
